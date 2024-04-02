16901efca0e515641eeec8c69a478595b696a6b5
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-04-02 16:24:49 +0000

    commit final

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
+const SimpleCounter = () => {
+  const [counter, setCounter] = useState(0);
+  const [pausarContador, setpausarContador] = useState(false);
 
-function SimpleCounter (props) {
-    return (
-        <p>hello world</p>
-    )
-}
+  useEffect(() => {
+    if (!pausarContador){
+    const interval = setInterval(() => {
+      setCounter(prevCounter => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }}, [pausarContador]);
+
+  const digitoHoras = () => Math.floor(counter / 3600);
+  const digitoMinutos = () => Math.floor((counter % 3600) / 60);
+  const digitoSegundos = () => counter % 60;
+
+  const ceroIzquierda = (number) => {
+    return number < 10 ? '0' + number : number.toString();
+  };
+
+  const botonPausar = () => {
+    setpausarContador(!pausarContador);
+  };
+
+  return (
+    <div>
+    <div className="ContenedorContador">
+      <div className="contenedorClock">
+      <div className="Calendar">
+        <i className="far fa-clock"></i>
+      </div>
+      </div>
+      <div className="contenedorHoras">
+        <div className="Tres">{ceroIzquierda(digitoHoras())} H</div>
+      </div>
+      <div className="contenedorMinutos">
+        <div className="Dos">{ceroIzquierda(digitoMinutos())} Min</div>
+      </div>
+      <div className="contenedorSegundos">
+        <div className="Uno">{ceroIzquierda(digitoSegundos())} Seg</div>
+      </div>
+    </div>
+    <div className="contenedorBotones">
+      <button onClick={botonPausar} className="botonPausar">{pausarContador ? 'Reanudar' : 'Pausar'}</button>
+    </div>
+    </div>
+  );
+};
 
 export default SimpleCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import SimpleCounter from ".src/js/component/SimpleCounter.jsx";
+import SimpleCounter from "./SimpleCounter.jsx";
 
 
 //create your first component
 const Home = () => {
 	return (
-		<SimpleCounter />
-	
+		<div className="container-fluid">
+		<div className="contenedorGrande">
+			<SimpleCounter />
+		</div>
+		</div>
 	);
 };
 
